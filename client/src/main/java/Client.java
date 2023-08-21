e5440b5a5eba2e01797f7fe538303f65781831cd
Dylan Bermudez Cardona <dyez1110@gmail.com>
2023-08-21 13:17:42 -0500

    feat(PrinterI): Add latency and exit

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,22 +1,24 @@
 import java.net.Inet4Address;
 import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "client.cfg")) {
 
+            // twoway añadido, debe esperar respuesta
             Demo.PrinterPrx chatManagerPrx = Demo.PrinterPrx
-                    .checkedCast(communicator.propertyToProxy("Printer.Proxy"));
+                    .checkedCast(communicator.propertyToProxy("Printer.Proxy")).ice_twoway();
 
             try {
                 String hostname = Inet4Address.getLocalHost().getHostName();
-                System.out.println("hostname: " + hostname);
                 String msg = "";
                 String username = System.getProperty("user.name");
 
                 while (!msg.equals("exit")){
                     msg = reader.nextLine();
                     chatManagerPrx.printString(username+":"+hostname+" "+msg);
+                    System.out.println(username+":"+hostname+" "+msg);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
--- server/src/main/java/PrinterI.java
+++ server/src/main/java/PrinterI.java
@@ -1,12 +1,29 @@
+import com.zeroc.Ice.Communicator;
+import com.zeroc.Ice.ObjectAdapter;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class PrinterI implements Demo.Printer {
+    // añadimos communicator para cerrar el server
+    Communicator communicator;
+    long start_time;
+
+    PrinterI(Communicator communicator){
+        this.communicator = communicator;
+    }
 
     public void printString(String msg, com.zeroc.Ice.Current current) {
+        System.out.println("\nExecuting -> " + msg);
+        start_time = System.currentTimeMillis();
+
         String[] msg_parts = msg.split(" ");
+
+        if(msg_parts[1].equalsIgnoreCase("exit")){
+            communicator.shutdown();
+        }
 
         String real_msg = msg_parts[1];
 
@@ -26,14 +43,16 @@ public class PrinterI implements Demo.Printer {
                 real_msg_parts = (real_msg + msg_parts[2]).split("listports");
                 msg_type = "listports";
             } else if(real_msg.contains("!")){
-                real_msg_parts = real_msg.split("!");
+                real_msg_parts = (real_msg + " " + msg_parts[2]).split("!");
                 msg_type = "!";
+            } else if(real_msg.equalsIgnoreCase("exit")){
+                msg_type = "exit";
             } else {
                 System.out.println("Ups! Type a valid message");
                 valid_msg = false;
             }
 
-            if(valid_msg){
+            if(valid_msg && !msg_type.equalsIgnoreCase("exit")){
                 if(verify_msg(real_msg_parts, msg_type)){
                     execute_command(real_msg_parts, msg_type);
                 } else {
@@ -44,21 +63,28 @@ public class PrinterI implements Demo.Printer {
     }
 
     public void prime_factors(int num){
+        String msg = "";
+
         while (num % 2 == 0) {
-            System.out.print(2 + " ");
+            // msg += 2 + " ";
+            System.out.println(2 + " ");
             num /= 2;
         }
 
         for (int i = 3; i <= Math.sqrt(num); i += 2) {
             while (num % i == 0) {
-                System.out.print(i + " ");
+                // msg += i + " ";
+                System.out.println(i + " ");
                 num /= i;
             }
         }
 
         if (num > 2) {
-            System.out.print(num);
+            // msg += num;
+            System.out.println(num);
         }
+
+        finished();
     }
 
     public boolean verify_msg(String[] real_msg_parts, String msg_type){
@@ -66,13 +92,13 @@ public class PrinterI implements Demo.Printer {
             case "listifs":
                 return (real_msg_parts.length == 0);
             case "listports":
-            case "!":
                 if(real_msg_parts.length == 2){
                     return (real_msg_parts[1].split(" ").length == 1);
                 } else {
                     return false;
                 }
-
+            case "!":
+                return (real_msg_parts.length == 2);
             default:
                 return false;
         }
@@ -107,5 +133,12 @@ public class PrinterI implements Demo.Printer {
         while ((resultExecution = br.readLine()) != null) {
             System.out.println(resultExecution);
         }
+        finished();
+    }
+
+    private void finished(){
+        long final_time = System.currentTimeMillis();
+        long latency = final_time - start_time;
+        System.out.println("Latency: " + latency +  "ms");
     }
 }
